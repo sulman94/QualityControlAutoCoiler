c0fb2d93625ed219e685c659bfa6917bfad3e3e5
Sulman khurshid <sulman.khurshid@codebtech.com>
2025-10-03 00:57:17 +0500

    FIxes

---
+++ Entities/ViewModels/DashboardSearchParams.cs
@@ -0,0 +1,33 @@
+﻿namespace Entities.ViewModels
+{
+    public class DashboardSearchParams
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string MachineId { get; set; }
+    }
+    public class PieChartSearchParams
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+    public class LineChartSearchParams
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+    public class LineChartSearchParamsById
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string MachineId { get; set; }
+    }
+}
